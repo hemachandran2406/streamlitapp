2224ea827ee5ab410c20d72c09458d46eb1373f9
hemachandran2406 <hema.g@pathsetter.io>
2024-06-06 12:38:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Simple Streamlit App')
+st.write('This is a simple Streamlit app embedded in a Power BI custom visual.')
+
+if st.button('Click me'):
+    st.write('Button clicked!')
+
